901d7e914d76a788b44649e503308243e75dc897
iNo_fensif <flytouchlucas@gmail.com>
2018-03-12 18:25:37 +0100

    Base du projet

---
+++ src/main/java/fr/llegrand/basictrack/MainActivity.java
@@ -0,0 +1,55 @@
+package fr.llegrand.basictrack;
+
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    NotificationManagerCompat notificationManager = null;
+    NotificationCompat.Builder mBuilder = null;
+    int i = 0;
+
+    private View.OnClickListener btnAddListener = new View.OnClickListener() {
+        public void onClick(View v) {
+            Log.i("CLICK", "BOUTON AJOUTER");
+
+            EditText prenom = (EditText) findViewById(R.id.prenom);
+            EditText nom = (EditText) findViewById(R.id.nom);
+            String person = prenom.getText() + " " + nom.getText();
+            Toast toast = Toast.makeText(getApplicationContext(), person + " a bien été ajouté!", Toast.LENGTH_SHORT);
+            toast.show();
+
+            i++;
+            if(i==3) {
+                notificationManager.notify(1, mBuilder.build());
+                i = 0;
+            }
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.i("INFO", "CONTENU SET");
+
+        Button button = (Button)findViewById(R.id.ajouter);
+        button.setOnClickListener(btnAddListener);
+
+        mBuilder = new NotificationCompat.Builder(this, "1")
+                .setContentTitle("Attention")
+                .setContentText("Merci de ne pas spammer le bouton")
+                .setSmallIcon(R.drawable.ic_smallicon)
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+        notificationManager = NotificationManagerCompat.from(this);
+    }
+}
