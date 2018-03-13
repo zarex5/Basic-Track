b1bffc4920bbeccecc38a7f442ef6f0b10a8efa4
iNo_fensif <flytouchlucas@gmail.com>
2018-03-13 23:39:43 +0100

    Ajout custom toolbar + menu

--- src/main/java/fr/llegrand/basictrack/MainActivity.java
+++ src/main/java/fr/llegrand/basictrack/MainActivity.java
@@ -9,13 +9,17 @@ import android.support.v4.app.NotificationCompat;
 import android.support.v4.app.NotificationManagerCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
     NotificationManagerCompat notificationManager = null;
     NotificationCompat.Builder mBuilder = null;
     int i = 0;
@@ -43,6 +47,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Log.i("INFO", "CONTENU SET");
+        toolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(toolbar);
 
         Button button = (Button)findViewById(R.id.ajouter);
         button.setOnClickListener(btnAddListener);
@@ -58,5 +64,32 @@ public class MainActivity extends AppCompatActivity {
                 .setContentIntent(pendingIntent)
                 .setAutoCancel(true);
         notificationManager = NotificationManagerCompat.from(this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        EditText nom = (EditText) findViewById(R.id.nom);
+        switch (item.getItemId()) {
+            case R.id.action_settings:
+                Log.i("DEBUG", "SETTINGS CLICKED");
+                nom.setText("");
+                return true;
+
+            case R.id.action_plus:
+                Log.i("DEBUG", "PLUS CLICKED");
+                nom.setText("Legrand");
+                return true;
+
+            default:
+                Log.i("DEBUG", "??? CLICKED");
+                return super.onOptionsItemSelected(item);
+
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
